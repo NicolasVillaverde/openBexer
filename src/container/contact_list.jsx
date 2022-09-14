9d18bcfb3d20b9063a0ca2822786b650bb367edc
Nicolas Villaverde <villaverdenicolaspablo@gmail.com>
2022-09-14 16:18:03 -0300

    New contact test

--- src/container/contact_list.jsx
+++ src/container/contact_list.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { Contact } from "../models/contact.class";
 import ContactComponent from "../pure/contact";
 const ContactListComponent = () => {
-  const defaultContact = new Contact("Luis", "Ramirez", "luisramirez@gmail.com", true);
+  const defaultContact = new Contact("Carlos", "Ramirez", "carlosramirez@gmail.com", false);
   return (
     <div>
       <ContactComponent contact={defaultContact}></ContactComponent>
